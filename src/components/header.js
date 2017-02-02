b2036ec4bebb746a239416247c69f0f18452635d
ridwanfansuri <ridwan@icehousecorp.com>
2017-02-02 15:38:12 +0700

    finish module 05 , add header

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//Import Library for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+//make a Component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+// Make the component available to other parts of the App
+export default Header;
