a26f0963fa75aaf810b971e649edf36e3adf706b
ridwanfansuri <ridwan@icehousecorp.com>
2017-02-02 15:38:39 +0700

    forget the android file

--- index.android.js
+++ index.android.js
@@ -2,13 +2,11 @@
 
 //import libraryto help create a Component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 //create a Component
 const App = () => (
-    <Text>
       <Header />
-    </Text>
   );
 
 //render it to the device
